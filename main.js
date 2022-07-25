b9f29d889c0150c5dda49916c312b8e78ad930f9
Sviatoslove <svyatpopov86@gmail.com>
2022-07-25 15:39:33 +0300

    firstcommit1

--- main.js
+++ main.js
@@ -55,10 +55,72 @@ let result = sumArgs(5);
 */
 
 const convertWord = (n = 0) => {
-  n % 100 / 10 === 1 ? console.log(`${n} Поваров`):
-  n % 10 === 1 && n !== 11 ? console.log(`${n} Повар`):
-  n % 10 === 2 && n !== 12 || n % 10 === 3 && n !== 13 || n % 10 === 4 && n !== 14 ? console.log(`${n} Повара`):
-  console.log(`${n} Поваров`);
+    let z = n % 100 / 10;
+    let x = n % 10;
+    z >= 1.1 && z <= 1.4 ? console.log(`${n} Поваров`):
+    x === 1 ? console.log(`${n} Повар`):
+    x === 2 || x === 3 || x === 4 ? console.log(`${n} Повара`):
+    console.log(`${n} Поваров`);
 };
-
-// convertWord();
+convertWord();
+convertWord(1);
+convertWord(2);
+convertWord(3);
+convertWord(4);
+convertWord(5);
+convertWord(6);
+convertWord(7);
+convertWord(8);
+convertWord(9);
+convertWord(10);
+convertWord(11);
+convertWord(12);
+convertWord(13);
+convertWord(14);
+convertWord(15);
+convertWord(16);
+convertWord(17);
+convertWord(18);
+convertWord(19);
+convertWord(20);
+convertWord(21);
+convertWord(22);
+convertWord(23);
+convertWord(24);
+convertWord(25);
+convertWord(26);
+convertWord(27);
+convertWord(28);
+convertWord(29);
+convertWord(30);
+convertWord(31);
+convertWord(32);
+convertWord(33);
+convertWord(34);
+convertWord(35);
+convertWord(36);
+convertWord(100);
+convertWord(101);
+convertWord(102);
+convertWord(103);
+convertWord(104);
+convertWord(105);
+convertWord(106);
+convertWord(999);
+convertWord(111);
+convertWord(112);
+convertWord(1112);
+convertWord(1113);
+convertWord(1114);
+convertWord(1115);
+convertWord(1116);
+convertWord(10000);
+convertWord(10001);
+convertWord(10002);
+convertWord(10003);
+convertWord(10004);
+convertWord(10005);
+convertWord(1000000);
+convertWord(1111111);
+convertWord(1111112);
+convertWord(1001015);
