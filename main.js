6caad4a023f788c621dbdaf9e198d53928f7d457
Sviatoslove <svyatpopov86@gmail.com>
2022-08-10 02:33:09 +0300

    furst commit2

--- main.js
+++ main.js
@@ -14,11 +14,13 @@ const requestUrl = 'https://jsonplaceholder.typicode.com/photos';
 const photosWrapper = document.querySelector('.photos__wrapper');
 const btnCreate = document.querySelector('.btnCreate');
 const btnUpdate = document.querySelector('.btnUpdate');
+const btnGet = document.querySelector('.btnGet');
 
 let btnsDelete;
 let photos = [];
+let checkClick = 0;
 
-const createTemplate = data => {
+const createTemplate = (data) => {
   return template = `
     <div class="wrapper__post" data-id="${data.id}">
         <div class="albumId">AlbumId: ${data.albumId}</div>
@@ -31,46 +33,58 @@ const createTemplate = data => {
   `
 };
 
-const getPhotos = url => {
+const getPhotos = (url, dozen) => {
   fetch(url)
     .then(response => response.json())
     .then(json => {
         photos = json;
         photos.filter(item => {
-        return item.id >= 50 && item.id <= 60;
+        return dozen > 0 ? item.id >= dozen * 10 + 1 && item.id <= (dozen * 10) + 10 : item.id >= dozen && item.id <= dozen + 9;
       }).forEach(photo => {
         photosWrapper.innerHTML += createTemplate(photo);
       });
-      btnsDelete = document.querySelectorAll('.btn__delete');
     })
     .then(() => {
-      for (const elem of btnsDelete) {
-        elem.addEventListener('click', e => {
-          const idElem = e.target.parentNode.dataset.id;
-          deletePhoto(requestUrl, idElem);
-        })
-      }
-    })
+      photosWrapper.append(btnGet);
+      let btnsGet = document.querySelectorAll('.btnGet');
+      btnsGet.forEach(item => {
+        if(btnsGet.length > 1) {
+          btnsGet[0].remove();
+        };
+      });
+      deletePhoto(requestUrl);
+    });
 };
 
-const deletePhoto = (url, id) => {
+const deletePhotoServer = (url, id) => {
   fetch(url + '/' + id, {
     method: 'DELETE'
-  })
+  });
 };
 
-getPhotos(requestUrl);
+const deletePhoto = url => {
+  btnsDelete = document.querySelectorAll('.btn__delete');
+  for (const elem of btnsDelete) {
+    elem.addEventListener('click', e => {
+      const idElem = e.target.parentNode.dataset.id;
+      e.target.parentNode.remove();
+      deletePhotoServer(url, idElem);
+    });
+  };
+};
+
+btnGet.addEventListener('click', e => {
+  e.preventDefault();
+  checkClick += 1;
+  getPhotos(requestUrl, checkClick - 1);
+});
 
 const createPhoto = url => {
-  let inputAlbumId = document.querySelector('.albumIdAdd').value;
-  let inputTitle = document.querySelector('.titleAdd').value;
-  let inputUrl = document.querySelector('.urlAdd').value;
-  let inputThumbnailUrl = document.querySelector('.thumbnailUrlAdd').value;
   let createObj = {
-    albumId: inputAlbumId,
-    title: inputTitle,
-    url: inputUrl,
-    thumbnailUrl: inputThumbnailUrl
+    albumId: document.querySelector('.albumIdAdd').value,
+    title: document.querySelector('.titleAdd').value,
+    url: document.querySelector('.urlAdd').value,
+    thumbnailUrl: document.querySelector('.thumbnailUrlAdd').value
   }
   fetch(url, {
     method: 'POST',
@@ -80,8 +94,13 @@ const createPhoto = url => {
     },
   })
     .then((response) => response.json())
-    .then((json) => console.log(json));
-}
+    .then((json) => {
+      photosWrapper.innerHTML += createTemplate(json);
+    })
+    .then(() => {
+      deletePhoto(url);
+    });
+};
 
 btnCreate.addEventListener('click', e => {
   e.preventDefault();
@@ -89,29 +108,35 @@ btnCreate.addEventListener('click', e => {
 })
 
 const updatePhoto = url => {
-  let inputAlbumIdUpdate = document.querySelector('.albumIdUpdate').value;
-  let inputTitleUpdate = document.querySelector('.titleUpdate').value;
-  let inputUrlUpdate = document.querySelector('.urlUpdate').value;
-  let inputThumbnailUrlUpdate = document.querySelector('.thumbnailUrlUpdate').value;
   let inputIdPhotoUpdate = document.querySelector('.idPhotoUpdate').value;
   fetch(url + '/' + inputIdPhotoUpdate, {
     method: 'PUT',
     body: JSON.stringify({
-        albumId: inputAlbumIdUpdate,
+        albumId: document.querySelector('.albumIdUpdate').value,
         id: inputIdPhotoUpdate,
-        title: inputTitleUpdate,
-        url: inputUrlUpdate,
-        thumbnailUrl: inputThumbnailUrlUpdate,
+        title: document.querySelector('.titleUpdate').value,
+        url: document.querySelector('.urlUpdate').value,
+        thumbnailUrl: document.querySelector('.thumbnailUrlUpdate').value,
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
     .then((response) => response.json())
-    .then((json) => console.log(json));
-}
+    .then((json) => {
+      let arrPhotos = document.querySelectorAll('.wrapper__post');
+      for(const photo of arrPhotos) {
+        if(json.id === (+ photo.dataset.id)) {
+          photo.children[0].innerHTML = document.querySelector('.albumIdUpdate').value;
+          photo.children[2].innerHTML = document.querySelector('.titleUpdate').value;
+          photo.children[3].innerHTML = document.querySelector('.urlUpdate').value;
+          photo.children[4].innerHTML = document.querySelector('.thumbnailUrlUpdate').value;
+        };
+      };
+    });
+};
 
 btnUpdate.addEventListener('click', e => {
   e.preventDefault();
   updatePhoto(requestUrl);
-})
+});
